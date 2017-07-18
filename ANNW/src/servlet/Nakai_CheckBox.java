53167f7e8fa33f814f5f1bca23b4f388b61ae92f
yuki-naki <yuki.naki1234@gmail.com>
2017-07-19 08:00:35 +0900

    haha

--- ANNW/src/servlet/Nakai_CheckBox.java
+++ ANNW/src/servlet/Nakai_CheckBox.java
@@ -21,7 +21,7 @@ public class Nakai_CheckBox extends HttpServlet {
         super();
     }
 
-	public  void doPosr(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	public  void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
 			req.setCharacterEncoding("UTF-8");
 			//名前,選択したものを取得
